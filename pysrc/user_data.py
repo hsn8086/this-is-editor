d921a0553e3d0d21bf5e636fbff89dff1638932f
hsn <hsn@zh314.xyz>
2025-08-29 05:33:46 +0800

    fix: update user data directory to use correct identifier

--- pysrc/user_data.py
+++ pysrc/user_data.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 import appdirs
 
-user_data_dir = Path(appdirs.user_data_dir("this_is_editor", "small-hsn"))
-user_config_dir = Path(appdirs.user_config_dir("this_is_editor", "small-hsn"))
+user_data_dir = Path(appdirs.user_data_dir("this_is_editor", "hsn8086"))
+user_config_dir = Path(appdirs.user_config_dir("this_is_editor", "hsn8086"))
 
 if not Path(user_config_dir).exists():
     Path(user_config_dir).mkdir(parents=True, exist_ok=True)
