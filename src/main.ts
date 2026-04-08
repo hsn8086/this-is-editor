a9c4113654897b84a74ec0922e3f82ef7d0b5444
hsn <hsn@zh314.xyz>
2026-04-08 22:06:39 +0800

    fix(input): disable spellcheck globally

--- src/main.ts
+++ src/main.ts
@@ -17,10 +17,20 @@ import App from './App.vue'
 // Styles
 import 'unfonts.css'
 
+function disableSpellcheck (): void {
+  document.body.setAttribute('spellcheck', 'false')
+  document
+    .querySelectorAll<HTMLInputElement | HTMLTextAreaElement>('input, textarea')
+    .forEach(element => {
+      element.spellcheck = false
+    })
+}
+
 function init () {
   const app = createApp(App)
   registerPlugins(app)
   app.mount('#app')
+  disableSpellcheck()
 }
 
 if (window.pywebview && window.pywebview.api) {
